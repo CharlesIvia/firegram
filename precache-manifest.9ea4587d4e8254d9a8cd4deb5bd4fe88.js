86957e2204cd02d53f785ed6eee95f5e2707a6e4
Charles Ivia <iviacharles@gmail.com>
2020-08-23 17:53:51 +0300

    Updates

---
+++ precache-manifest.9ea4587d4e8254d9a8cd4deb5bd4fe88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394e3e9d626d6ffa9ef2dee34c22259c",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "97ae69a3ad3b45cc7561",
+    "url": "/firegram/static/css/main.69b19522.chunk.css"
+  },
+  {
+    "revision": "0ea188f0a2eff732af6e",
+    "url": "/firegram/static/js/2.6aeb846b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.6aeb846b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ae69a3ad3b45cc7561",
+    "url": "/firegram/static/js/main.0854b10f.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
